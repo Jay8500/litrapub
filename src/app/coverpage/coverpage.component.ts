a7fdeb3e27911b1e3dc4bab53156c1184d44c180
Jaya Krishna Lakkoju <64889966+Jay8500@users.noreply.github.com>
2025-05-06 19:44:01 +0530

    litera changes

--- src/app/coverpage/coverpage.component.ts
+++ src/app/coverpage/coverpage.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
 @Component({
   selector: 'litra-coverpage',
-  imports: [CardModule],
+  imports: [CardModule, TagModule],
   templateUrl: './coverpage.component.html',
-  styleUrl: './coverpage.component.css'
+  styleUrl: './coverpage.component.css',
 })
-export class CoverpageComponent {
-
-}
+export class CoverpageComponent {}
